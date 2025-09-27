289eea372f140859f6ac3cfff9cae23baf47bb67
kavinda-100 <kavindarathnayake100@gmail.com>
2025-09-27 11:12:45 +0530

    Add NFT minting functionality and mapping to store NFTs by owner

--- contract/src/CryptoCanvasNFT.sol
+++ contract/src/CryptoCanvasNFT.sol
@@ -9,7 +9,17 @@ import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/ext
  * @notice This contract hold the logic of NFTs.
  */
 contract CryptoCanvasNFT is ERC721URIStorage {
+    // -------------------------- Events ------------------------------
+    event NFTMinted(address indexed creator, uint256 indexed tokenId, string tokenURI);
+
     // -------------------------- State Variables ------------------------------
+    struct NFTs {
+        uint256 tokenId;
+        string tokenURI;
+    }
+
+    mapping(address => NFTs[]) allNFTs; // Mapping to store all NFTs by owner
+
     uint256 private _nextTokenId; // token count
 
     constructor() ERC721("CryptoCanvasNFT", "CCNFT") {}
@@ -23,8 +33,14 @@ contract CryptoCanvasNFT is ERC721URIStorage {
     function mintNFT(string memory tokenURI) external returns (uint256) {
         uint256 newItemId = _nextTokenId++;
 
+        // Mint the NFT and set the token URI
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
+        // Store the NFT details in the mapping
+        allNFTs[msg.sender].push(NFTs({tokenId: newItemId, tokenURI: tokenURI}));
+
+        // Emit the minting event
+        emit NFTMinted(msg.sender, newItemId, tokenURI);
 
         return newItemId;
     }
@@ -38,6 +54,15 @@ contract CryptoCanvasNFT is ERC721URIStorage {
      */
     function getTokenURI(uint256 tokenId) external view returns (string memory) {
         return tokenURI(tokenId);
+    }
+
+    /**
+     * @notice This function returns all NFTs with tokenURIs that belong to a specific address
+     * @param _user address of the user
+     * @return array of NFTs struct containing tokenId and tokenURI
+     */
+    function getNFTsWithTokenURIByUser(address _user) external view returns (NFTs[] memory) {
+        return allNFTs[_user];
     }
 
     /**

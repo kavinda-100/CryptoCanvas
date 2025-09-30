ee3602628871259d87e21b520d4de122b68549d6
kavinda-100 <kavindarathnayake100@gmail.com>
2025-09-30 12:26:38 +0530

    Add Treasury tests for NFT minting, approval, listing, and withdrawal functionality

---
+++ contract/test/unit/TreasuryTest.t.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Treasury} from "../../src/Treasury.sol";
+import {CryptoCanvasNFT} from "../../src/CryptoCanvasNFT.sol";
+import {MarketPlace} from "../../src/MarketPlace.sol";
+import {Deploy} from "../../script/Deploy.s.sol";
+
+contract TreasuryTest is Test {
+    Treasury private treasury;
+    CryptoCanvasNFT private nft;
+    MarketPlace private marketplace;
+    address public user1 = makeAddr("user");
+    address public user2 = makeAddr("user2");
+
+    function setUp() external {
+        // deploy the contract
+        (nft, marketplace, treasury) = new Deploy().run();
+
+        // fund
+        vm.deal(user1, 10 ether);
+        vm.deal(user2, 10 ether);
+    }
+
+    // -------------------------- Treasury tests ------------------------------
+
+    /**
+     * @notice Test to withdraw money as owner
+     */
+    function test_Withdraw_money_as_owner() external {
+        // Mint NFT as user1
+        vm.startPrank(user1);
+        string memory tokenURI = "ipfs://example-token-uri";
+        uint256 tokenId = nft.mintNFT(tokenURI);
+        vm.stopPrank();
+
+        // Approve marketplace to transfer the NFT, then list NFT for sale as user1
+        vm.startPrank(user1);
+        nft.approve(address(marketplace), tokenId); // approval of NFT to marketplace
+        uint256 price = 1 ether;
+        marketplace.listNFT(address(nft), tokenId, price);
+        vm.stopPrank();
+
+        // buy NFT as user2
+        vm.startPrank(user2);
+        marketplace.buyNFT{value: price}(1); // assume listingId is 1
+        vm.stopPrank();
+
+        // calculate fee
+        uint256 feeAmount = (price * marketplace.getFeePercent()) / 100; // goes to treasury
+        // Calculate seller amount
+        uint256 sellerAmount = price - feeAmount;
+
+        // Check treasury balance
+        assertEq(address(treasury).balance, feeAmount);
+        // Check user1 balance (seller)
+        assertEq(user1.balance, 10 ether + sellerAmount);
+        // Check user2 balance (buyer)
+        assertEq(user2.balance, 10 ether - price);
+
+        // Withdraw money as owner
+        address owner = treasury.owner();
+        uint256 ownerInitialBalance = owner.balance;
+        vm.prank(owner);
+        treasury.withdraw(owner);
+        // Check treasury balance is 0
+        assertEq(address(treasury).balance, 0);
+        // Check owner balance increased
+        assertEq(owner.balance, ownerInitialBalance + feeAmount);
+    }
+}

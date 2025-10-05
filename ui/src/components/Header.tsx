798fd54234a454f1c8a98334164c846e50074bcc
kavinda-100 <kavindarathnayake100@gmail.com>
2025-10-05 12:07:20 +0530

    feat: add Sheet component and integrate it into Header for mobile navigation

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -8,8 +8,21 @@ import { ModeToggle } from "./ModeToggle";
 import { cn } from "@/lib/utils";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
 
-const navItems = [
+type NavItem = {
+  label: string;
+  href: string;
+};
+
+const navItems: NavItem[] = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about" },
   { label: "Explore", href: "/explore" },
@@ -82,10 +95,83 @@ export const Header = () => {
           accountStatus={"avatar"}
           chainStatus={"icon"}
         />
-        {/* mode toggle */}
-        <ModeToggle />
-        {/* mobile menu */}
+        <div className="hidden md:block">
+          {/* mode toggle */}
+          <ModeToggle />
+        </div>
+
+        <div className="md:hidden">
+          {/* mobile menu */}
+          <MobileMenu navItems={navItems} />
+        </div>
       </div>
     </header>
+  );
+};
+
+const MobileMenu = ({ navItems }: { navItems: NavItem[] }) => {
+  const pathname = usePathname();
+  const account = useAccount();
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <MenuIcon className="h-6 w-6" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle className="sr-only">CryptoCanvas</SheetTitle>
+        </SheetHeader>
+        {/* header */}
+        <div className="mb-4 flex items-center justify-between">
+          <SheetHeader>
+            <SheetTitle>CryptoCanvas</SheetTitle>
+          </SheetHeader>
+          {/* mode toggle */}
+          <ModeToggle />
+        </div>
+        <div>
+          {/* navigation */}
+          <div className="flex w-full flex-col gap-6 px-3">
+            {navItems.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className={cn(
+                  "text-md hover:text-primary w-full rounded-sm border px-3 py-2 font-medium transition-colors",
+                  pathname === item.href ? "text-primary" : "text-secondary",
+                )}
+              >
+                {item.label}
+              </Link>
+            ))}
+            {/* only show if account is connected */}
+            {account.isConnected && (
+              <>
+                <Link
+                  key="/My Art"
+                  href="/my-art"
+                  className={cn(
+                    "text-md hover:text-primary w-full rounded-sm border px-3 py-2 font-medium transition-colors",
+                    pathname === "/my-art" ? "text-primary" : "text-secondary",
+                  )}
+                >
+                  My Art
+                </Link>
+                <Link
+                  key="/create"
+                  href="/create"
+                  className={cn(
+                    "text-md hover:text-primary w-full rounded-sm border px-3 py-2 font-medium transition-colors",
+                    pathname === "/create" ? "text-primary" : "text-secondary",
+                  )}
+                >
+                  Create
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };

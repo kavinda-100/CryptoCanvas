4bdd38dc0b4d0b186506805fbc462c817366c1f3
kavinda-100 <kavindarathnayake100@gmail.com>
2025-10-08 11:07:12 +0530

    feat: Add MyArtPage component with account connection check and redirect

---
+++ ui/src/app/my-art/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useAccount } from "wagmi";
+
+export const MyArtPage = () => {
+  const account = useAccount();
+  const router = useRouter();
+
+  // Redirect to home if not connected
+  React.useEffect(() => {
+    if (
+      (!account.isConnected && account.status !== "connecting") ||
+      account.isReconnecting
+    ) {
+      router.push("/");
+    }
+  }, [account, router]);
+
+  return <div>MyArtPage</div>;
+};

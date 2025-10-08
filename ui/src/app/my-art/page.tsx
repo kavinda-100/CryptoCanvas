66b7264832dd5f24e19668ed845620abf624caa4
kavinda-100 <kavindarathnayake100@gmail.com>
2025-10-08 11:24:24 +0530

    feat: Implement MyArtPage to display active NFTs with loading and error handling

--- ui/src/app/my-art/page.tsx
+++ ui/src/app/my-art/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useGetSellerActiveNFTs } from "@/hooks/useGetSellerActiveNFTs";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { useAccount } from "wagmi";
@@ -18,7 +19,39 @@ const MyArtPage = () => {
     }
   }, [account, router]);
 
-  return <div>MyArtPage</div>;
+  // get the active nfts listed by the user
+  const {
+    activeNFTs,
+    isActiveNFTsPending,
+    isActiveNFTsError,
+    ActiveNFTsError,
+  } = useGetSellerActiveNFTs(account.address!);
+
+  // handle loading state
+  if (isActiveNFTsPending) {
+    return <div>Loading...</div>;
+  }
+
+  // handle error state
+  if (isActiveNFTsError) {
+    return <div>Error: {ActiveNFTsError?.message}</div>;
+  }
+
+  // handle empty state
+  if (activeNFTs.length === 0) {
+    return <div>No active NFTs found.</div>;
+  }
+
+  return (
+    <section className="w-full">
+      {activeNFTs.map((nft) => (
+        <div key={nft.listingId.toString()} className="mb-4 border p-4">
+          <h2 className="text-lg font-bold">{nft.tokenURI}</h2>
+          <p>Price: {nft.price.toString()} wei</p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default MyArtPage;

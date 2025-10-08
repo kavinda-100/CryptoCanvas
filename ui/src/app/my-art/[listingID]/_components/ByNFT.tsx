23f481630b0a7142f05691dd0e3dea7447935cd0
kavinda-100 <kavindarathnayake100@gmail.com>
2025-10-08 20:11:16 +0530

    feat: add ByNFT and CancelNFTListing components with respective buttons

---
+++ ui/src/app/my-art/[listingID]/_components/ByNFT.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export const ByNFT = () => {
+  return (
+    <div>
+      <Button>Buy NFT</Button>
+    </div>
+  );
+};
---
+++ ui/src/app/my-art/[listingID]/_components/CancelNFTListing.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export const CancelNFTListing = () => {
+  return (
+    <div>
+      <Button variant="destructive">Cancel NFT Listing</Button>
+    </div>
+  );
+};

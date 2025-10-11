676cc8628355fccb2a39a404e2edf56e7289e9f2
kavinda-100 <kavindarathnayake100@gmail.com>
2025-10-11 11:30:03 +0530

    Update deployment addresses for CryptoCanvas NFT to Sepolia network

--- ui/src/abi/index.ts
+++ ui/src/abi/index.ts
@@ -1,12 +1,25 @@
+// local deployment ---------------------------------------------------------------
+
 // "cryptoCanvasNFT_address": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
 //   "cryptoCanvasNFT_marketPlace_address": "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
 //   "cryptoCanvasNFT_treasury_address": "0x5FbDB2315678afecb367f032d93F642f64180aa3",
 
-// local deployment
+// export const CRYPTO_CANVAS_NFT_ADDRESS =
+//   "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+// export const CRYPTO_CANVAS_NFT_MARKETPLACE_ADDRESS =
+//   "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+// export const CRYPTO_CANVAS_NFT_TREASURY_ADDRESS =
+//   "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+// Sepolia deployment ---------------------------------------------------------------
+
+// "cryptoCanvasNFT_address": "0x0a2F221A8F531eeF97822AC1232F520F55C8DfB6",
+// "cryptoCanvasNFT_marketPlace_address": "0xcE7B306e1C80F81A65EdA9886E837c2Dedf2bF11",
+// "cryptoCanvasNFT_treasury_address": "0xfeBF366200eD4664acb14a4466a012e72e43c22c",
 
 export const CRYPTO_CANVAS_NFT_ADDRESS =
-  "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  "0x0a2F221A8F531eeF97822AC1232F520F55C8DfB6";
 export const CRYPTO_CANVAS_NFT_MARKETPLACE_ADDRESS =
-  "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  "0xcE7B306e1C80F81A65EdA9886E837c2Dedf2bF11";
 export const CRYPTO_CANVAS_NFT_TREASURY_ADDRESS =
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  "0xfeBF366200eD4664acb14a4466a012e72e43c22c";

183177959ec13d7cc758f895cdcaa6e30572500a
kavinda-100 <kavindarathnayake100@gmail.com>
2025-10-11 11:34:08 +0530

    Refactor config to use Sepolia network and environment variables for project ID and RPC URL

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,18 +1,20 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  anvil,
-  sepolia,
-} from "wagmi/chains";
+import { sepolia } from "wagmi/chains";
+import { http } from "wagmi";
+
+// get envs
+const ALCHEMY_RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL;
+const WALLET_CONNECT_PROJECT_ID =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
 
 const config = getDefaultConfig({
   appName: "CryptoCanvas",
-  projectId: "YOUR_PROJECT_ID",
-  chains: [mainnet, polygon, optimism, arbitrum, sepolia, anvil],
+  projectId: WALLET_CONNECT_PROJECT_ID,
+  chains: [sepolia],
   ssr: true, // If your dApp uses server side rendering (SSR)
+  transports: {
+    [sepolia.id]: http(ALCHEMY_RPC_URL),
+  },
 });
 
 export default config;
